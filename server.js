edd4b27145c9866bfd9af0dd4af9f5e42deb4e07
Art Ramirez <artramirez08@gmail.com>
2023-02-12 09:53:44 -0800

    sending index and show data

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+// MIDDLEWARE
+app.use(express.static('public'))
 
 
 // ROUTES
@@ -17,10 +19,15 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads')
 })
-
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
+//404
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
+
 
 // LISTEN
 app.listen(PORT, () => {
