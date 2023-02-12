a8f5311c3687ba3c16b0fb5fa06881964a7605d2
Art Ramirez <artramirez08@gmail.com>
2023-02-12 10:46:14 -0800

    create and new routes complete

--- views/index.jsx
+++ views/index.jsx
@@ -3,19 +3,27 @@ const Default = require('./layouts/default')
 
 function Index({breads, title}) {
     return (
-        <Default title = {title}>
-            <h2>Index Page</h2>
-            <p>I have {breads[0].name} bread!</p>
-            {/*JSX Comment*/}
-            <ul>
-                {
-                    breads.map((bread,index)=>{
-                        return (<li key={index}><a href={`/breads/${index}`}>{bread.name}</a></li>)
-                    })
-                }
-            </ul>
-        </Default>
-    )
+      <Default title={title}>
+        <h2>Index Page</h2>
+        <p>I have {breads[0].name} bread!</p>
+        {/*JSX Comment*/}
+        <ul>
+          {breads.map((bread, index) => {
+            return (
+              <li key={index}>
+                <a href={`/breads/${index}`}>{bread.name}</a>
+              </li>
+            );
+          })}
+        </ul>
+        
+        <div className="newButton">
+          <a href="/breads/new">
+            <button>Add a new bread</button>
+          </a>
+        </div>
+      </Default>
+    );
 }
 
 module.exports = Index
